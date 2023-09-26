2f8f8b82d3d4562de0ab11cf1be1d87726e4bc33
zgibson6464 <102778567+zgibson6464@users.noreply.github.com>
2023-09-25 20:33:04 -0700

    button expirimentation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 interface Props {
   children: string;
+  color?: "primary" | "secondary" | "danger";
   onClick: () => void;
 }
 
-const Button = ({ children, onClick }: Props) => {
+const Button = ({ children, onClick, color = "primary" }: Props) => {
   return (
-    <button className="btn btn-primary" onClick={onClick}>
-      My Button
+    <button className={"btn btn-" + color} onClick={onClick}>
+      {children}
     </button>
   );
 };
